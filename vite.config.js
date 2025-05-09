fdae5dea0c3a62b01b9a23cbbdeaea23b1da4749
simon <Simon.hinojosa01@Unicatolica.edu.co>
2025-05-09 03:23:20 -0500

    Ajuste para subirlo al vercel

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000
-  },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
     minify: true,
-    target: 'es2015'
+    chunkSizeWarningLimit: 1000
   }
 })
